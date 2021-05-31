3bb55158007eb5a2756cbdc142f21fae9bc7790f
cmd-exe-web <aakashverma7184@gmail.com>
2021-05-31 17:05:53 +0530

    first

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '4PZDOQ6KvU'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'sa1903@srmist.edu.in'
+    MAIL_PASSWORD = 'SRMWorld@#13978151'
